d81284ae0d21663fa5267312f58069d3474e903a
Walter Schulze <awalterschulze@gmail.com>
2024-08-11 19:02:10 +0100

    remove global constants

--- lean/Examples/JessicaCantSwim/Player.lean
+++ lean/Examples/JessicaCantSwim/Player.lean
@@ -3,14 +3,17 @@ open Examples.JessicaCantSwim.Keys
 
 namespace Examples.JessicaCantSwim.Player
 
-private def speed : Float := 200
-private def radius : Float := 10
-
 structure Player where
   private position : Vector2
+  private speed: Float
+  private radius : Float
 
 def Player.init (position: Vector2): Player :=
-  { position := position }
+  {
+    position := position,
+    speed := 200,
+    radius := 10,
+  }
 
 private def Player.modifyPositionX (p: Player) (f : Float → Float) : Player :=
   { p with position := ⟨ f p.position.x, p.position.y ⟩}
@@ -20,7 +23,7 @@ private def Player.modifyPositionY (p: Player) (f : Float → Float) : Player :=
 
 def Player.update (p: Player) (delta : Float) (keys: List Keys.Keys): Id Player := do
   let mut p := p
-  let move := speed * delta
+  let move := p.speed * delta
   if (keys.contains Keys.Left) then p := p.modifyPositionX (· - move)
   if (keys.contains Keys.Right) then p := p.modifyPositionX (· + move)
   if (keys.contains Keys.Up) then p := p.modifyPositionY (· - move)
@@ -29,6 +32,6 @@ def Player.update (p: Player) (delta : Float) (keys: List Keys.Keys): Id Player 
 
 -- IO is required, since we are drawing
 def Player.render (p: Player): IO Unit := do
-  drawCircleV p.position radius Color.green
+  drawCircleV p.position p.radius Color.green
 
 end Examples.JessicaCantSwim.Player
