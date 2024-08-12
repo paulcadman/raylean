0c297e791d30df59594e0306ab0e308924d4ab52
Walter Schulze <awalterschulze@gmail.com>
2024-08-12 17:28:43 +0100

    jessica - entities (broken)

--- lean/Examples/JessicaCantSwim/Entity.lean
+++ lean/Examples/JessicaCantSwim/Entity.lean
@@ -3,6 +3,7 @@ import Examples.JessicaCantSwim.Keys
 namespace Entity
 
 inductive ID where
+  | All
   | Player
   | Scoreboard
   | Ocean
@@ -12,10 +13,69 @@ inductive Event where
   | Collision (src: ID) (dst: ID) : Event
   | Key (key: Keys.Keys) : Event
 
-class Entity (E : Type) where
+class Entity (E : Type u) where
   id (entity: E): ID
   update (entity: E) (delta : Float) (events: List Event) : E
   bounds (entity: E): List Rectangle
   render (entity: E): IO Unit
+
+inductive Elem where
+  | mk (elem: Σ α, Entity α × α): Elem
+
+def Entities := List Elem
+
+def wrap [Entity E] (e: E): Elem :=
+  Elem.mk ⟨ E, ⟨ inferInstance, e ⟩ ⟩
+
+def Elem.id (elem: Elem): ID :=
+  match elem with
+  | Elem.mk ⟨_, ⟨_, entity ⟩ ⟩ => Entity.id entity
+
+def Elem.bounds (elem: Elem): List Rectangle :=
+  match elem with
+  | Elem.mk ⟨_, ⟨_, entity ⟩ ⟩ => Entity.bounds entity
+
+def Elem.render (elem: Elem): IO Unit :=
+  match elem with
+  | Elem.mk ⟨_, ⟨_, entity ⟩ ⟩ => Entity.render entity
+
+def Elem.update (elem: Elem) (delta : Float) (events: List Event) : Elem :=
+  match elem with
+  | Elem.mk ⟨_, ⟨_, entity ⟩ ⟩ => wrap <| Entity.update entity delta events
+
+instance : Entity Elem where
+  id := Elem.id
+  update := Elem.update
+  bounds := Elem.bounds
+  render := Elem.render
+
+def Entities.forM [Monad m] : Entities → (Elem → m PUnit) → m PUnit
+  | [], _ => pure ()
+  | x :: xs, action => do
+    action x
+    forM xs action
+
+instance : ForM m Entities Elem where
+  forM := Entities.forM
+
+def Entities.id (_entities: Entities): ID :=
+  ID.All
+
+def Entities.bounds (entities: Entities): List Rectangle :=
+  match entities with
+  | [] => []
+  | x :: xs => List.append x.bounds (Entities.bounds xs)
+
+def Entities.render (entities: Entities): IO Unit :=
+  forM entities (λ elem => elem.render)
+
+def Entities.update (entities: Entities) (delta : Float) (events: List Event) : Entities :=
+  List.map (λ entity => entity.update delta events) entities
+
+instance : Entity Entities where
+  id := Entities.id
+  update := Entities.update
+  bounds := Entities.bounds
+  render := Entities.render
 
 end Entity
--- lean/Examples/JessicaCantSwim/Game.lean
+++ lean/Examples/JessicaCantSwim/Game.lean
@@ -12,48 +12,40 @@ namespace Game
 
 structure Game where
   camera : Camera.Camera
-  player : Player.Player
-  scoreboard : Scoreboard.Scoreboard
-  ocean : Ocean.Ocean
+  entities : Entity.Entities
 
 def init (position: Vector2) (screenWidth: Nat) (screenHeight: Nat): Game :=
   let camera := Camera.init position screenWidth screenHeight
-  let player := Player.init position
-  let scoreboard := Scoreboard.init
-  let ocean := Ocean.init screenWidth screenHeight
+
+  let player := Entity.wrap <| Player.init position
+  let scoreboard := Entity.wrap <| Scoreboard.init
+  let ocean := Entity.wrap <| Ocean.init screenWidth screenHeight
+  let entities := [player, scoreboard, ocean]
+
   {
     camera := camera,
-    player := player,
-    scoreboard := scoreboard,
-    ocean := ocean,
+    entities := entities,
   }
 
 def Game.detectCollisions (game: Game): List Entity.Event :=
-  let entities : List (Entity.ID × List Rectangle) :=
-    [
-      (game.player.id, game.player.bounds),
-      (game.scoreboard.id, game.scoreboard.bounds),
-      (game.ocean.id, game.ocean.bounds),
-    ]
-  let collisions := Collision.detects entities
+  let idBoundPairs : List (Entity.ID × List Rectangle) :=
+    List.map (λ entity => (entity.id, entity.bounds)) game.entities
+  let collisions := Collision.detects idBoundPairs
   List.map (λ collision => Entity.Event.Collision collision.1 collision.2) collisions
 
 def Game.update (game: Game) (delta : Float) (events: List Entity.Event): Game :=
   let collisions := game.detectCollisions
   let allEvents := List.append events collisions
+  let updatedEntities := List.map (λ entity => entity.update delta allEvents) game.entities
   {
     camera := game.camera,
-    player := game.player.update delta allEvents,
-    ocean := game.ocean.update delta allEvents,
-    scoreboard := game.scoreboard.update delta allEvents,
+    entities := updatedEntities
   }
 
 def Game.render (game: Game): IO Unit := do
   clearBackground Color.Raylib.lightgray
   renderWithCamera2D game.camera.camera do
-    game.player.render
-    game.ocean.render
-    game.scoreboard.render
+    game.entities.render
   return ()
 
 end Game
