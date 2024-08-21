6f6f196f8c153612e4a40f850bd72356e2807b06
Walter Schulze <awalterschulze@gmail.com>
2024-08-21 10:45:09 +0100

    jessica - simplify entities

--- lean/Examples/JessicaCantSwim/Game.lean
+++ lean/Examples/JessicaCantSwim/Game.lean
@@ -12,6 +12,7 @@ namespace Game
 
 structure Game where
   camera : Camera.Camera
+  -- Add your new Entity here:
   player: Player.Player
   ocean: Ocean.Ocean
   scoreboard: Scoreboard.Scoreboard
@@ -20,38 +21,37 @@ def init (position: Vector2) (screenWidth: Nat) (screenHeight: Nat): Game :=
   let camera := Camera.init position screenWidth screenHeight
   {
     camera := camera,
-    -- Add your new Entity:
+    -- Add your new Entity here:
     player := Player.init position,
     scoreboard := Scoreboard.init,
     ocean := Ocean.init screenWidth screenHeight,
   }
 
-def Game.entities (game: Game): Entity.Entities :=
-  Entity.Entities.mk [
-    -- Add your new Entity:
-    Entity.wrap <| game.player,
-    Entity.wrap <| game.scoreboard,
-    Entity.wrap <| game.ocean,
-  ]
-
 private def Game.update (game: Game) (delta : Float) (events: List Entity.Event): Game :=
   {
     camera := game.camera,
-    -- Add your new Entity:
+    -- Add your new Entity here:
     player := game.player.update delta events
     scoreboard := game.scoreboard.update delta events
     ocean := game.ocean.update delta events
   }
 
-def Game.step (game: Game) (delta : Float) (externalEvents: List Entity.Event): Game :=
-  let collisions := Collision.detectEvents game.entities
-  let allEvents := List.append externalEvents collisions
-  Game.update game delta allEvents
-
 def Game.render (game: Game): IO Unit := do
   clearBackground Color.Raylib.lightgray
   renderWithCamera2D game.camera.camera do
-    game.entities.render
+    -- Add your new Entity here:
+    game.ocean.render
+    game.player.render
+    game.scoreboard.render
   return ()
+
+def Game.step (game: Game) (delta : Float) (externalEvents: List Entity.Event): Game :=
+  let collisions := Collision.detectEvents <| Entity.Entities.mk [
+    -- Add your new Entity here:
+    Entity.wrap <| game.ocean,
+    Entity.wrap <| game.player
+  ]
+  let allEvents := List.append externalEvents collisions
+  Game.update game delta allEvents
 
 end Game
