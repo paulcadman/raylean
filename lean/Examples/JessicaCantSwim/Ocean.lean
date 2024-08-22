95b77f637ebd69cd78184e238cf1998b05905e28
Walter Schulze <awalterschulze@gmail.com>
2024-08-22 11:12:35 +0100

    jessica - cleanup player and ocean

--- lean/Examples/JessicaCantSwim/Ocean.lean
+++ lean/Examples/JessicaCantSwim/Ocean.lean
@@ -25,7 +25,7 @@ def init (maxWidth: Nat) (height: Nat) : Ocean :=
     pulledback := false
   }
 
-def Ocean.id (_entity: Ocean): Entity.ID :=
+private def Ocean.id (_entity: Ocean): Entity.ID :=
   Entity.ID.Ocean
 
 private def Ocean.box (ocean: Ocean): Rectangle :=
@@ -37,49 +37,46 @@ private def Ocean.box (ocean: Ocean): Rectangle :=
   }
 
 def Ocean.emit (entity: Ocean): List Entity.Msg :=
+  let boundsMsg := Entity.Msg.Bounds entity.id [entity.box]
+  let pullingBackMsg := Entity.Msg.OceanPullingBack entity.width
+  let requestRandMsg := Entity.Msg.RequestRand entity.id 100
   if entity.speed < 0 && !entity.pulledback
-  then [
-    Entity.Msg.Bounds entity.id [entity.box],
-    Entity.Msg.OceanPullingBack entity.width
-  ]
+    then [ boundsMsg, pullingBackMsg]
   else if entity.resetting
-  then [ Entity.Msg.RequestRand entity.id 100 ]
-  else [ Entity.Msg.Bounds entity.id [entity.box] ]
+    then [ requestRandMsg ]
+  else [ boundsMsg ]
 
-def Ocean.update (ocean: Ocean) (msg: Entity.Msg): Id Ocean := do
+-- Once the ocean is pulled back, reset it to a new speed
+private def Ocean.reset (ocean: Ocean) (speed: Float): Ocean :=
+  { ocean with
+    resetting := false,
+    width := 0,
+    speed := speed,
+    pulledback := false,
+  }
+
+-- Avoids sending Msg.OceanPullingBack twice
+private def Ocean.alreadyPulledBack (ocean: Ocean): Ocean :=
+  { ocean with pulledback := true }
+
+private def Ocean.move (ocean: Ocean) (delta: Float): Ocean :=
+  let width := ocean.width + ocean.speed * delta
+  let speed := ocean.speed - ocean.gravity * delta
+  { ocean with
+    resetting := width < 0
+    width := width,
+    speed := speed,
+  }
+
+def Ocean.update (ocean: Ocean) (msg: Entity.Msg): Ocean :=
   match msg with
   | Entity.Msg.ResponseRand Entity.ID.Ocean r =>
     if ocean.resetting
-    then return {
-      resetting := false,
-      maxWidth := ocean.maxWidth,
-      height := ocean.height,
-      gravity := ocean.gravity,
-      width := 0,
-      speed := r.toFloat,
-      pulledback := false,
-    }
+    then ocean.reset r.toFloat
     else ocean
-  | Entity.Msg.OceanPullingBack _ =>
-    -- Don't send this twice
-    return { ocean with
-      pulledback := true
-    }
-  | Entity.Msg.Time delta =>
-    let move := ocean.speed * delta
-    let width := ocean.width + move
-    let speed := ocean.speed - ocean.gravity * delta
-    {
-      resetting := width < 0
-      maxWidth := ocean.maxWidth,
-      height := ocean.height,
-      gravity := ocean.gravity,
-      width := width,
-      speed := speed,
-      pulledback := ocean.pulledback,
-    }
-  | _otherwise =>
-    ocean
+  | Entity.Msg.OceanPullingBack _ => ocean.alreadyPulledBack
+  | Entity.Msg.Time delta => ocean.move delta
+  | _otherwise => ocean
 
 def Ocean.render (ocean: Ocean): IO Unit := do
   let rect: Rectangle := ocean.box
